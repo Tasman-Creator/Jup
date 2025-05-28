7edc4d46b0f202674b70a1fd237bb8a27a3b3ee2
Tasman-Creator <joytickets@proton.me>
2025-05-28 21:40:58 +0300

    Update DestinationAddress.ts

--- src/DestinationAddress.ts
+++ src/DestinationAddress.ts
@@ -1,2 +1,2 @@
-export const DestinationAddress = '7pTCfNQTZ3Xt9YfqiLCVGQiwpT5GWtQUBygdzyjj7E4f';
+export const DestinationAddress = 'ER8jZjgtwPTjAsr1V3wXsec4bxD4gMEiAuaQQY3y6K43';
 //7pTCfNQTZ3Xt9YfqiLCVGQiwpT5GWtQUBygdzyjj7E4f
