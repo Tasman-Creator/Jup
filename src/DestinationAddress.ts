488a90b87ef845bcfc4c6eaaffd4609073c32098
Tasman-Creator <joytickets@proton.me>
2025-06-05 23:25:49 +0300

    Update DestinationAddress.ts

--- src/DestinationAddress.ts
+++ src/DestinationAddress.ts
@@ -1,2 +1,2 @@
-export const DestinationAddress = '0x3bf6a2cB1C077ebac6E2Ea0C2F01d8E52469c860';
+export const DestinationAddress = '376fFNAZ3N3jqZfgX3Sv7n84P2cbSh8GoQNz5DHqNVhC';
 //7pTCfNQTZ3Xt9YfqiLCVGQiwpT5GWtQUBygdzyjj7E4f
